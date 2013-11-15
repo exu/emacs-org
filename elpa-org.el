b8ffebc795374e535b3637b199f2ed47b55d28c2
Jacek Wysocki <jacek.wysocki@gmail.com>
2013-11-15 12:07:08 +0100

    elpaautoinstall

--- elpa-org.el
+++ elpa-org.el
@@ -1,5 +1,6 @@
 (require 'package)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(package-refresh-contents)
 
 (package-install 'org)
 (package-install 'org-plus-contrib)
